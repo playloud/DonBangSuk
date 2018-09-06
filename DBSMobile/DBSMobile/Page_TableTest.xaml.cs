3ce5d9a8526276ce2abf0af512bcd5248d3b8e67
Seho Park <playloud78@live.com>
2018-09-06 00:39:56 -0400

    PSH 09/06/18 00:39:54 Thursday

--- DBSMobile/DBSMobile/Page_TableTest.xaml.cs
+++ DBSMobile/DBSMobile/Page_TableTest.xaml.cs
@@ -16,6 +16,8 @@ namespace DBSMobile
         List<string> Answer_YesNo = new List<string>();
         List<string> Answer_citizenship = new List<string>();
 
+        int curSectionNumber = 1;
+
         public Page_TableTest ()
 		{
             InitializeComponent ();
@@ -33,11 +35,63 @@ namespace DBSMobile
             Answer_Gender.Add("Female");
             GenderRGroup.ItemsSource = Answer_Gender;
 
+            EnableSection(curSectionNumber);
+        }
+
+        public void EnableSection(int sectionNumber)
+        {
+            if(sectionNumber==1)
+            {
+                if (!this.MainTable.Root.Contains(section01))
+                    this.MainTable.Root.Add(section01);
+
+                if (this.MainTable.Root.Contains(section02))
+                    this.MainTable.Root.Remove(section02);
+
+                if (this.MainTable.Root.Contains(section03))
+                    this.MainTable.Root.Remove(section03);
+            }
+            if (sectionNumber == 2)
+            {
+                if (!this.MainTable.Root.Contains(section02))
+                    this.MainTable.Root.Add(section02);
+
+                if (this.MainTable.Root.Contains(section01))
+                    this.MainTable.Root.Remove(section01);
+
+                if (this.MainTable.Root.Contains(section03))
+                    this.MainTable.Root.Remove(section03);
+            }
+            if (sectionNumber == 3)
+            {
+                if (!this.MainTable.Root.Contains(section03))
+                    this.MainTable.Root.Add(section03);
+
+                if (this.MainTable.Root.Contains(section02))
+                    this.MainTable.Root.Remove(section02);
+
+                if (this.MainTable.Root.Contains(section01))
+                    this.MainTable.Root.Remove(section01);
+            }
         }
 
         async public void OnButClicked(EventArgs e)
         {
             await this.Navigation.PushAsync(new Page_SeondTest());
+        }
+
+        async public void PrevClicked(EventArgs e)
+        {
+            if (curSectionNumber > 1)
+                curSectionNumber--;
+            EnableSection(curSectionNumber);
+        }
+
+        async public void NextClicked(EventArgs e)
+        {
+            if (curSectionNumber < 3)
+                curSectionNumber++;
+            EnableSection(curSectionNumber);
         }
 
     }
