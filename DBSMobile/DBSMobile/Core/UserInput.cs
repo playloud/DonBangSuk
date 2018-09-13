7701bc60fb1f531c23e58080b9f26d352baf6cdc
Seho Park <playloud78@live.com>
2018-09-13 00:40:46 -0400

    for update

---
+++ DBSMobile/DBSMobile/Core/UserInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBSMobile.Core
+{
+    public class UserInput
+    {
+        //
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+}
